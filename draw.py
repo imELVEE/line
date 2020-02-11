cfaa76e04c849010feb4693bc8b8dbd812d2ff70
Alvi Aziz <aaziz00@stuy.edu>
2020-02-11 03:50:17 -0500

    check

--- draw.py
+++ draw.py
@@ -33,7 +33,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
             d2 += (2*a)
 
     #OCTANT 2 AND 6
-    elif (1 < slope and b == 0):
+    if (1 < slope and b == 0):
         #midpoint times 2
         d2 = 2 * b + a
         #loop to make line

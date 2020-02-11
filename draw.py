d5fc923e8c64d4be1a0412db5c08fe8aefd6e087
Alvi Aziz <aaziz00@stuy.edu>
2020-02-11 04:50:17 -0500

    try

--- draw.py
+++ draw.py
@@ -25,17 +25,15 @@ def draw_line( x0, y0, x1, y1, screen, color ):
         #midpoint times 2
         d2 = 2 * a + b
         #loop to make line
-        if (currentx > x1):
-            currenty = y1
-            x1 = x0
-        while currentx <= x1:
-            plot(screen,color,currentx,currenty)
-            d0 = 2*a*(currentx+1) + 2*b*(currenty+1) + c
-            if abs(d0) < abs(d2):
-                currenty += 1
-                d2 += (2*b)
-            currentx += 1
-            d2 += (2*a)
+        if (currentx <= x1):
+            while currentx <= x1:
+                plot(screen,color,currentx,currenty)
+                d0 = 2*a*(currentx+1) + 2*b*(currenty+1) + c
+                if abs(d0) < abs(d2):
+                    currenty += 1
+                    d2 += (2*b)
+                    currentx += 1
+                    d2 += (2*a)
 
     #OCTANT 2 AND 6
     elif (1 <= slope):

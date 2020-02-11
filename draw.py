b6b2b7a8004a5e61b825ca022afb106172464e13
Alvi Aziz <aaziz00@stuy.edu>
2020-02-11 03:38:35 -0500

    added case for detecting octant 1

--- draw.py
+++ draw.py
@@ -3,6 +3,11 @@ from display import *
 #d0 is pixel to the up-right
 #d1 if pixel to the right
 
+# octants 1 and 5 : 0 < m < 1
+# octants 2 and 6 : 1 < m < undefined
+# octants 3 and 7 : undefined < m < -1
+# octants 4 and 8 : -1 < m < 0
+
 def draw_line( x0, y0, x1, y1, screen, color ):
     currentx = x0
     currenty = y0
@@ -13,17 +18,15 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     b = -1 * (x1 - x0)
     c = b * ((a / b) * (0 - x0) + y0)
 
-    d2 = 2 * a + b
-
-    while currentx <= x1:
-        plot(screen,color,currentx,currenty)
-        d0 = 2*a*(currentx+1) + 2*b*(currenty+1) + c
-        if abs(d0) < abs(d2): #possible error
-            currenty += 1
-            d2 += (2*b)
-        currentx += 1
-        d2 += (2*a)
-
-
-def formula(slope,x0,y0,x):
-    return slope * (x - x0) + y0
+    if (0 <= slope and slope <= 1):
+        #midpoint times 2
+        d2 = 2 * a + b
+        #loop to make line
+        while currentx <= x1:
+            plot(screen,color,currentx,currenty)
+            d0 = 2*a*(currentx+1) + 2*b*(currenty+1) + c
+            if abs(d0) < abs(d2):
+                currenty += 1
+                d2 += (2*b)
+                currentx += 1
+                d2 += (2*a)

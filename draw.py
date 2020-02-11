aae4b2807e4367262fce42c757e16b708cf58ead
Alvi Aziz <aaziz00@stuy.edu>
2020-02-11 04:02:32 -0500

    fixed indent and test octant 3

--- draw.py
+++ draw.py
@@ -33,7 +33,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
             d2 += (2*a)
 
     #OCTANT 2 AND 6
-elif (1 <= slope):
+    elif (1 <= slope):
         #midpoint times 2
         d2 = 2 * b + a
         #loop to make line
@@ -45,3 +45,17 @@ elif (1 <= slope):
                 d2 += (2*a)
             currenty += 1
             d2 += (2*b)
+
+    #OCTANT 3 AND 7
+    elif (slope < -1):
+        #midpoint times 2
+        d2 = 2 * b + a
+        #loop to make line
+        while currenty >= y1:
+            plot(screen,color,currentx,currenty)
+            d0 = 2*a*(currentx+1) + 2*b*(currenty-1) + c
+            if abs(d0) > abs(d2):
+                currentx += 1
+                d2 += (2*a)
+            currenty -= 1
+            d2 -= (2*b)
